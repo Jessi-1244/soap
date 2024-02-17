cea74d7206c6f41daffc5670bc934ac3f7c8157d
Jessi-1244 <146610362+Jessi-1244@users.noreply.github.com>
2024-02-18 02:05:52 +0530

    Delete salespages.js
    Signed-off-by: Jessi-1244 <146610362+Jessi-1244@users.noreply.github.com>

--- salespages.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("sales").addEventListener("click", function() {
-        document.getElementById("salesTable").style.display = "table";
-    });
-});
